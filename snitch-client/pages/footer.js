334a70324ee360ea301e1fa21aabd5cbbece4b99
Daryl Rogony <darylrogony@gmail.com>
2022-10-22 11:09:09 +0300

    Fit: adjust styling

--- snitch-client/pages/footer.js
+++ snitch-client/pages/footer.js
@@ -2,20 +2,30 @@
 function Footer({}) {
   return (
     <div>
-    <div class="container-fluid">
-    <footer class="bg-dark text-center text-white">
-    <div class="container p-4 pb-0">
+    <div class="container-fluid" background-color="#9054F7">
+    <footer class="bg-success text-center text-white">
+    <div class="container p-5 pb-0">
       <section class="">
         <form action="">
           <div class="row d-flex justify-content-center">
+             <div class="col-auto">
+              <h1 class="pt-2">
+                <strong>Snitch</strong>
+              </h1>
+             </div>
+             <div class="col-auto">
+              <h1 class="pt-2">
+                <strong>Menu</strong>
+              </h1>
+             </div>
             <div class="col-auto">
               <p class="pt-2">
-                <strong>Sign up for our newsletter</strong>
+                <strong>Subscribe to our monthly newsletter</strong>
               </p>
             </div>
             <div class="col-md-5 col-12">
               <div class="form-outline form-white mb-4">
-                <input type="email" id="form5Example2" class="form-control" />
+                <input type="email" id="form5Example2" class="form-control"  />
                 <label class="form-label" for="form5Example2">Email address</label>
               </div>
             </div>
@@ -30,7 +40,7 @@ function Footer({}) {
     </div>
     <div class="text-center p-3">
       © 2020 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      <a class="text-white" href="">Snitch.com</a>
     </div>
   </footer>
 </div>
