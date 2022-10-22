610cfcc154922da60c6d91375f4fea5023f9ba34
George <gnjuki19@gmail.com>
2022-10-22 16:44:32 +0300

    Fixed the about page styles

--- snitch-client/pages/about.js
+++ snitch-client/pages/about.js
@@ -5,11 +5,8 @@ function About() {
     return (
 
       <div class="container-fluid mt-3">
-  <h1>Two Unequal Responsive Columns</h1>
-  <p>Resize the browser window to see the effect.</p>
-  <p>The columns will automatically stack on top of each other when the screen is less than 576px wide.</p>
   <div class="row">
-    <div class="col-sm-5 p-3 bg-primary text-white">
+    <div class="col-sm-5 p-3 text-dark">
     <h5 style={{
       fontFamily: 'Acme',
       fontStyle: 'normal',
@@ -20,7 +17,26 @@ function About() {
       color: '#000000'}}>A few words about this blog platform,  <br /> 
        Snitch, and how this site was made</h5>
     </div>
-    <div class="col-sm-7 p-3 bg-warning text-white">.col</div>
+    <div class="col-sm-7 p-3 text-dark">
+    <div class="container">
+  <div class ="row justify-content-between">
+    <div class="col-4">
+    <img src="https://pilbox.themuse.com/image.jpg?filter=antialias&h=384&opt=1&pos=top-left&prog=1&q=keep&url=https%3A%2F%2Fcms-assets.themuse.com%2Fmedia%2Flead%2Fquestions-to-ask-05202022-602012447-Thomas-Barwick.jpg%3Fv%3Ded8d97d1eee7834c77adc5d87afe41247a8b2473&w=767"
+        alt="Holding image"
+        width="70px"
+        height="70px" />
+    </div>
+    <div class="col-4">  <h5>Mika Matikainen</h5>
+        <p>Apr 15, 2020 · 4 min read</p></div>
+    <div class="col-4">Social media icons</div>
+  </div>
+</div>
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit tempus erat egestas efficitur. In hac habitasse platea dictumst. Fusce a nunc eget ligula suscipit finibus. Aenean pharetra quis lacus at viverra. 
+
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam quis posuere ligula. In eu dui molestie, molestie lectus eu, semper lectus. </p>
+        </div>
+    </div>
   </div>
 </div>
 
@@ -28,6 +44,24 @@ function About() {
 }
 
 export default About
+
+
+{/* <div class="container">
+  <div class ="row">
+    <div class="col-3">
+    <img src="https://pilbox.themuse.com/image.jpg?filter=antialias&h=384&opt=1&pos=top-left&prog=1&q=keep&url=https%3A%2F%2Fcms-assets.themuse.com%2Fmedia%2Flead%2Fquestions-to-ask-05202022-602012447-Thomas-Barwick.jpg%3Fv%3Ded8d97d1eee7834c77adc5d87afe41247a8b2473&w=767"
+        alt="Holding image"
+        width="70px"
+        height="70px" />
+    </div>
+    <div class="col-3">  <h5>Mika Matikainen</h5>
+        <p>Apr 15, 2020 · 4 min read</p></div>
+    <div class="col-3">Social media icons</div>
+  </div>
+</div> */}
+
+
+
     // < h1 >
     //     A few words about this blog platform,<br/> Snitch, and how this site was made
     //   </h1>
@@ -35,7 +69,7 @@ export default About
 
 
 
-    //     <div className="container-fluid">
+//         <div className="container-fluid">
 //     <div className="row" style={{ backgroundColor: "gray" }}>
 //       <div className="col-sm-6">
 //         <h2>A few words about this blog platform,  <br /> 
