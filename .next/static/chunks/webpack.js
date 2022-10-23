97e9fd445f23f424ffdb2bb60898f43259709188
Stensis <irenenjuguna98@gmail.com>
2022-10-23 13:20:44 +0300

    added changes to reflect development branch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "667a04950b98f953"; }
+/******/ 		__webpack_require__.h = function() { return "da1b7d41ea47ee2f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
