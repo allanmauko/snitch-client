f585b5a324ae43d09f474e056df28b4c5b5d420d
Stensis <irenenjuguna98@gmail.com>
2022-10-26 15:17:10 +0300

    changes

--- snitch-client/pages/_app.js
+++ snitch-client/pages/_app.js
@@ -1,12 +1,9 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
-// import 'sweetalert2/src/sweetalert2.scss'
-
-
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} 
-  />
+
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- snitch-client/pages/index.js
+++ snitch-client/pages/index.js
@@ -1,21 +1,32 @@
 import Head from 'next/head'
-import App from './home'
-import styles from '../styles/Home.module.css'
+import Banner from '../sections/banner';
+import Header from '../components/header/header';
+// import {Alert} from 'theme-ui';
+// import About from '../sections/about';
+// import Services from '../sections/services';
+// import Team from '../sections/team';
+// import Footer from '../sections/footer';
+import Page from './home/page';
+import Login from './login/login';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
-      <Head>
-        <App/>
-      </Head>
-      <main className={styles.main}>
-      
-      </main>
+      <div>
+        <Head>
+          <title>Responsive Travel Website</title>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet"/>
 
-      <footer className={styles.footer}>
-        
-      </footer>
-    </div>
+          <link rel="icon" href="/pro.ico" />
+        </Head>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+        <Page />
+        {/*<Header/>*/}
+        {/*<Banner />*/}
+        {/*<About />*/}
+        {/*<Services/>*/}
+        {/*<Team/>*/}
+        {/*<Footer/>*/}
+      </div>
   )
 }
