765605ad27d76c6c588d5bd8e367fd4779ea276c
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-10-26 15:24:10 +0300

    pull changes

--- snitch-client/pages/home.js
+++ snitch-client/pages/home.js
@@ -4,16 +4,18 @@ import About from "../pages/about";
 import ArticleCard from "./articleCard";
 import Footer from "./footer";
 import Login from "./login";
+import Dashboard from "../pages/admin/dashboard"
 
 function App() {
   return (
     <div>
-          <Navbar />
-          <Login />
-          <Banner />
-          <About />
-          <ArticleCard />
-          <Footer />
+          {/*<Navbar />*/}
+          {/*<Login />*/}
+          {/*<Banner />*/}
+          {/*<About />*/}
+          {/*<ArticleCard />*/}
+          {/*<Footer />*/}
+      <Dashboard />
     </div>
   );
 }
