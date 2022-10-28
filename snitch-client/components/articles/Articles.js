ce0699eda3f0d40125d0118281c1cfc9ff2dea7a
George <gnjuki19@gmail.com>
2022-10-28 12:54:50 +0300

    Adeed the articles page and mapped them in the app component

---
+++ snitch-client/components/articles/Articles.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Article from './Article'
+function Articles({articles, removeArticle}) {
+  return (
+    <>
+        <div>
+          <h1 style={{width: "200%",
+  margin: "auto", paddingLeft: "275px", paddingBottom: "20px", fontWeight: "700"}}>What to read next...</h1>
+        </div>
+        <div>
+          {articles.map((article) => {
+            return (
+              <Article key={article.id} {...article} removeArticle={removeArticle} />
+            )
+          })}
+        </div>
+        {/* <Article /> */}
+    </>
+
+  )
+}
+
+export default Articles
+
+
--- snitch-client/pages/home/page.js
+++ snitch-client/pages/home/page.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/header/header';
 import Banner from '../../sections/banner';
 import About from '../../sections/about';
-import Services from '../../sections/services';
+// import Services from '../../sections/services';
 import Team from '../../sections/team';
 import Footer from '../../sections/footer';
+import Loading from '../../components/articles/Loading';
+import Articles from '../../components/articles/Articles';
 // import UserSignUp from '../userSignUp';
 
 const Page = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('https://635aa5516f97ae73a632efe8.mockapi.io/blogs')
+    .then(res => res.json())
+    .then(data => {
+      setLoading(false)
+      setArticles(data)
+    })
+    .catch(error => {
+      setLoading(false)
+      console.log(error)
+    })
+  }, [])
+
+  // console.log(articles)
+
+    function removeArticle(id){
+    const updatedArticles = articles.filter((article) => article.id !== id)
+    setArticles(updatedArticles)
+  }
+
+  if(loading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+
   return (
       <div>
         <Header />
         {/* <UserSignUp/> */}
         <Banner />
         <About />
-        <Services/>
+        {/* <Services/> */}
+        <Articles articles={articles} removeArticle={removeArticle} />
         <Team/>
         <Footer/>
       </div>
