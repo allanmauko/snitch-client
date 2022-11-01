83b4aac2c4e7a960bc9ab01cad9b38868168f805
allanmauko <allan.mauko@student.moringaschool.com>
2022-11-01 14:29:24 +0300

    Revert "bug fix"
    This reverts commit 005b9095d47662e8d1c85f4d9ef44af397efff14.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "07a0e00d256754c3"; }
+/******/ 		__webpack_require__.h = function() { return "5ee0e353958bfa2f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
