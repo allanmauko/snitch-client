fcf03d23655b3dd18c5bba95be499cabdf8df636
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-11-03 14:34:37 +0300

    aricles

--- snitch-client/components/articles/Articles.js
+++ snitch-client/components/articles/Articles.js
@@ -3,34 +3,34 @@ import Article from "./Article";
 // import Services from "../../sections/services";
 function Articles({ articles, removeArticle, setData }) {
   return (
-    <>
-      <div>
-        <h1
-          style={{
-            width: "200%",
-            margin: "auto",
-            paddingLeft: "275px",
-            paddingBottom: "20px",
-            fontWeight: "700",
-          }}
-        >
-        
-        </h1>
-      </div>
-      <div>
-        {articles.map((article) => {
-          return (
-            <Article
-              key={article.id}
-              {...article}
-              removeArticle={removeArticle}
-              setData={setData}
-            />
-          );
-        })}
-      </div>
-      {/* <Article /> */}
-    </>
+      <>
+        <div>
+          <h1
+              style={{
+                width: "200%",
+                margin: "auto",
+                paddingLeft: "275px",
+                paddingBottom: "20px",
+                fontWeight: "700",
+              }}
+          >
+
+          </h1>
+        </div>
+        <div>
+          {articles.map((article) => {
+            return (
+                <Article
+                    key={article.id}
+                    {...article}
+                    removeArticle={removeArticle}
+                    setData={setData}
+                />
+            );
+          })}
+        </div>
+        {/* <Article /> */}
+      </>
   );
 }
 export default Articles;
